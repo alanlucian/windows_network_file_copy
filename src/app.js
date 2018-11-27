82f11425277a57f210d5db19f3574c295a3af463
alanlucian <alanlucian@gmail.com>
2018-11-27 15:40:28 -0200

    Initial Commit adding files

---
+++ src/app.js
@@ -0,0 +1,67 @@
+const path = require('path');
+var fs = require('fs');
+var networkData = JSON.parse(fs.readFileSync('network.json', 'utf8'));
+//const networkData = require('./network.json');
+
+var cmd=require('node-cmd');
+
+const now = new Date();
+const BKP_TIMESTAMP =  `${now.getFullYear()}${now.getMonth()}${now.getDate()}_${now.getHours()}${now.getMinutes()}${now.getSeconds()}` ;
+
+// const IGNORE_FILE = __dirname + path.sep + 'ignore.txt'
+
+
+
+
+cmd.get('net use',(err, data, stderr)=>{
+    //console.log(err, data, stderr);
+});
+
+
+let completeCopyCount = 0;
+
+for( var i = 0 ; i < networkData.destiny.length ;  i++  ){
+
+    let copyToPath = path.sep+path.sep +  networkData.destiny[i].ip +  ( !networkData.destiny[i].hasOwnProperty('path') ?   networkData.globalPath : networkData.destiny[i].path ) ;
+    let bkpToPath = path.sep+path.sep +  networkData.destiny[i].ip +  ( !networkData.destiny[i].hasOwnProperty('bkpPath') ?   networkData.globalBkpPath : networkData.destiny[i].bkpPath ) ;
+    var authCmd = '';
+    if( networkData.destiny[i].hasOwnProperty('auth') ){
+        var authData = networkData.destiny[i].auth == 'global' ?   networkData.globalAuth : networkData.destiny[i].auth;
+        var user = authData.user;
+        var pwd = authData.pwd;
+        authCmd = `net use ${copyToPath}  /USER:${user} ${pwd} && `;
+    }
+    let bkpFolder = bkpToPath + path.sep + `bkp_${BKP_TIMESTAMP}` ;
+    cmd.get(`mkdir ${bkpFolder} `, (err, data, stderr)=>{
+        //var moveCmd = `xcopy  ${copyToPath}${path.sep}*  ${bkpFolder}\\ `; WORK NO DIR
+        var bkpCmd = `xcopy  ${copyToPath}  ${bkpFolder}  /Z /Y /S /J`;
+        
+        console.log( `Executando BKP [${bkpCmd}]` );
+        cmd.get(bkpCmd, (err, data, stderr)=>{
+            if( err != null){
+                console.log('>>>',err,data,stderr);
+                return;
+            }
+            var copyCmd = authCmd +  "xcopy "+ networkData.origin +" " + copyToPath + " /Z /Y /S /J";
+            console.log( `Executando Cópia [${copyCmd}]` );
+            cmd.get(
+                copyCmd,
+                function(err, data, stderr){
+                    if(err!=null){
+                        console.log("!ERR!\n\n\t",err, data, stderr)
+                    }else{
+                        completeCopyCount++;
+                        console.log( `Cópia para ${copyToPath} concluída ( ${completeCopyCount} / ${networkData.destiny.length} )`  )
+                    }
+                    
+                }
+            )
+        });
+    });
+    
+    
+
+}
+
+console.log(BKP_TIMESTAMP);
+
